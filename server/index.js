d2f4f0576523090fa2c25261864373281f05ea42
Alexandre <alexandre.benoist@epitech.eu>
2025-01-10 11:34:37 +0100

    feat(server): /users /nick /create /join /list /delete commands

--- server/index.js
+++ server/index.js
@@ -36,7 +36,69 @@ io.on('connection', (socket) => { // When a user connects
 
     // When a msg is sent, broadcast it to all users
     socket.on('chat message', async (msg) => {
-        io.to(currentRoom).emit('chat message', name + ": " + msg);
+        if (msg.startsWith("/")) {
+            const command = msg.split(" ")[0];
+            switch (command) {
+                //List all users
+                case "/users":
+                    socket.emit('chat message', "list of users : " + users);
+                    break;
+                // Change name
+                case "/nick":
+                    users[users.indexOf(name)] = msg.split(" ")[1];
+                    name = msg.split(" ")[1];
+                    socket.emit('chat message', "Your name has been changed to " + name);
+                    break;
+                //create
+                case "/create":
+                    const room = msg.split(" ")[1];
+                    rooms.push(room);
+                    socket.join(room);
+                    io.emit('chat message', "New room : " + room + " has been created by " + name);
+                    break;
+                //join
+                case "/join":
+                    const roomToJoin = msg.split(" ")[1];
+                    if (!rooms.includes(roomToJoin)) {
+                        socket.emit('chat message', name + ": Room not found");
+                        break;
+                    } else {
+                        socket.leave(currentRoom);
+                        socket.join(roomToJoin);
+                        currentRoom = roomToJoin;
+                        io.to(roomToJoin).emit('chat message', name + " joined the room");
+                    }
+                    break;
+                //list rooms
+                case "/list":
+                    socket.emit('chat message', "list of rooms : " + rooms);
+                    break;
+                //delete room
+                case "/delete":
+                    const roomToDelete = msg.split(" ")[1];
+                    if (!rooms.includes(roomToDelete)) {
+                        socket.emit('chat message', name + ": Room not found");
+                        break;
+                    } else {
+                        rooms = rooms.filter(item => item !== roomToDelete);
+                        io.emit('chat message', "Room : " + roomToDelete + " has been deleted by " + name);
+                        const socketsInRoom = await io.in(roomToDelete).fetchSockets();
+                        socketsInRoom.forEach((socket) => {
+                            socket.leave(roomToDelete);
+                            socket.join("default");
+                            currentRoom = "default";
+                        });
+                        io.to("default").emit('chat message', 'Users from room ' +roomToDelete + ' have been moved to the default room.`);
+                        break;
+                    }
+                default:
+                    socket.emit('chat message', name + ": Command not found");
+                    break;
+            }
+        } else {
+            io.to(currentRoom).emit('chat message', name + ": " + msg);
+        }
+
     });
 });
 
